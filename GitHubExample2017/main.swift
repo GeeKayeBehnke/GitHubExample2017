84f14b6d0fc4863b67f76757330acd7da9ceba5d
behnke <behnke@behnkes-macbook-pro-2.local>
2017-12-06 21:38:09 -0600

    Initial Commit

---
+++ GitHubExample2017/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubExample2017
+//
+//  Created by behnke on 12/6/17.
+//  Copyright © 2017 behnke. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
